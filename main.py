8ff1543886c686b3c7b3a13cabd1558faaca75c7
renandevcode <renanramos1326@icloud.com>
2026-05-15 07:45:48 -0300

    [feat]:Implementando function calls com ollama

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from src.scrap_cobertura import (
     calcular_media_estados,
     baixar_e_tratar_dados,
 )
+from tools.tools import tools
 from src.buscar_postos import buscar_postos_proximos,threading_search,start_drivers
 import src.notify as notify
 from src.auxiliares import gerar_botoes_vacinas, calcular_data_alvo, definir_categoria_por_idade, converter_periodo_para_meses, validar_data
@@ -562,107 +563,6 @@ def faq_reactions(msg):
 
 
 # LLM
-
-
-tools = [
-
-    {"type": "function",
-"function": {
-    "name": "servicos",
-    "description": (
-        "Fornece servicos disponiveis com botoes : 'Início','Vacinas','Cobertura Vacinal','Unidades próximas','FAQ' "
-        "Para ver barra de servicos em botoes , semelhante ao menu "
-    ),
-    "parameters": {
-        "type": "object",
-        "properties": {}
-            }
-        }
-    },
-{"type": "function",
-"function": {
-    "name": "comandos",
-    "description": (
-        "É o primeiro serviço a enviar quando alguém entra, então cumprimentos e mensagens sem sentido podem retornar a função "
-        "Primeira interação do usuário ou o reinicio das atiividades"
-    ),
-    "parameters": {
-        "type": "object",
-        "properties": {}
-            }
-        }
-    }
-    ,
-    {
-        "type": "function",
-        "function": {
-            "name": "pedir_localizacao",
-            "description": (
-                "Solicita a localização do usuário para gerar unidades de saúde próoxima onde ele possa se vacinar "
-                "para encontrar UBS ou vacinas próximas"
-            ),
-            "parameters": {
-                "type": "object",
-                "properties": {}
-            }
-        }
-    },
-{
-        "type": "function",
-        "function": {
-            "name": "escolher_regiao",
-            "description": (
-                "fornece regiões para verificar a cobertura vacinal"
-                "para ver cobertura vacinal da  regiao"
-            ),
-            "parameters": {
-                "type": "object",
-                "properties": {}
-            }
-        }
-    },
-
-{
-        "type": "function",
-        "function": {
-            "name": "dashboard",
-            "description": (
-                "fornecer dashboard com dados de cobertura vacinal"
-                "para ver um dashboard interativo das coberturas vacinais "
-            ),
-            "parameters": {
-                "type": "object",
-                "properties": {}
-            }
-        }
-    },
-{
-    "type": "function",
-    "function": {
-        "name": "processar_dados",
-        "description": (
-            "Use esta função quando o usuário informar ou quiser informar "
-            "sua data de nascimento para consultar vacinas recomendadas, "
-            "vacinas pendentes, calendário vacinal ou verificar sua situação vacinal. "
-            "A data deve estar no formato DD/MM/AAAA."
-        ),
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "data_nascimento": {
-                    "type": "string",
-                    "description": (
-                        "Data de nascimento do usuário no formato DD/MM/AAAA. "
-                        "Exemplo: 25/12/2010"
-                    )
-                }
-            },
-            "required": ["data_nascimento"]
-        }
-    }
-}
-
-]
 @bot.message_handler(func=lambda m: True)
 def linguagem_natural(message):
     response = ollama.chat(
